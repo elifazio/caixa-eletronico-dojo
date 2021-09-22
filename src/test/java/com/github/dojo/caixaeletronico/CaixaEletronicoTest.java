023e9d09f40c1b4efda5f5646d33f373e539feca
Elifazio Bernardes <elifazio.silva@bradesco.com.br>
2021-09-22 15:07:34 -0300

    Primeira sessão de dojo com 1h e 8 participantes...

--- src/test/java/com/github/dojo/caixaeletronico/CaixaEletronicoTest.java
+++ src/test/java/com/github/dojo/caixaeletronico/CaixaEletronicoTest.java
@@ -1,7 +1,14 @@
 package com.github.dojo.caixaeletronico;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 /**
  * Classe de teste para Dojo do desafio Caixa Eletrônico
@@ -16,4 +23,67 @@ public class CaixaEletronicoTest {
     public void setUp() {
         caixaEletronico = new CaixaEletronico();
     }
+
+    @Test()
+    public void sacar_valor_10(){
+        BigDecimal valorSaque = new BigDecimal(10);
+       Assert.assertEquals(10,  caixaEletronico.sacar(valorSaque));
+
+    }    
+
+    // @Test
+    // public void sacar_sucesso() {
+    //     BigDecimal valorSaque = new BigDecimal(30);
+    //     List<Integer> notas = caixaEletronico.sacar(valorSaque);
+
+
+        
+    //     assertEquals(valorSaque, );
+    //     assertEquals(1, notas.qtdDez);
+    //     assertEquals(0, notas.qtdCinquenta);
+    //     assertEquals(0, notas.qtdCem);
+    // }
+
+    // @Test
+    // public void sacar_envia_nota_5_indisponivel(){
+
+    //     BigDecimal valorSaque = new BigDecimal(5);
+
+    //     assertFalse((valorSaque % 10 == 0));
+    // }
+
+    // @Test
+    // public void sacar_envia_nota_25_indisponivel(){
+
+    //     BigDecimal valorSaque = new BigDecimal(25);
+
+    //     assertFalse((valorSaque % 10 == 0));
+    // }   
+
+    // @Test
+    // public void sacar_envia_nota_212_indisponivel(){
+
+    //     BigDecimal valorSaque = new BigDecimal(212);
+
+    //     assertFalse((valorSaque % 10 == 0));
+    // } 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_envia_nota_0_indisponivel(){
+        BigDecimal valorSaque = new BigDecimal(0);
+        caixaEletronico.sacar(valorSaque);
+    }          
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_valor_decimal(){
+        BigDecimal valorSaque = new BigDecimal(1.20);
+        caixaEletronico.sacar(valorSaque);
+    }          
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_valor_negativo(){
+        BigDecimal valorSaque = new BigDecimal(-1);
+        caixaEletronico.sacar(valorSaque);
+    }    
+
 }
