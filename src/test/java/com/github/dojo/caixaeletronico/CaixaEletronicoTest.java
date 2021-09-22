a2d57736df405bce60966bbe78fc4b33cd430602
Elifazio Bernardes <elifazio.silva@bradesco.com.br>
2021-09-22 06:25:24 -0300

    Criação do projeto para execução do dojo....

---
+++ src/test/java/com/github/dojo/caixaeletronico/CaixaEletronicoTest.java
@@ -0,0 +1,24 @@
+package com.github.dojo.caixaeletronico;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe de teste para Dojo do desafio Caixa Eletrônico
+ * 
+ * Desafio Caixa Eletrônico: https://dojopuzzles.com/problems/caixa-eletronico/
+ */
+public class CaixaEletronicoTest {
+
+    private CaixaEletronico caixaEletronico;
+
+    @Before
+    public void setUp() {
+        caixaEletronico = new CaixaEletronico();
+    }
+
+    @Test
+    public void test() {
+        
+    }
+}
