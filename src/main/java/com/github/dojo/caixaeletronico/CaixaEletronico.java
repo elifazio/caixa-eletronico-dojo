99dfaa4f6edfa768c80b7ecc96f1c0e8185fffa6
Elifazio Bernardes <elifazio.silva@bradesco.com.br>
2021-09-29 15:14:59 -0300

    Criado os cenários básicos para os valores de cada nota...

--- src/main/java/com/github/dojo/caixaeletronico/CaixaEletronico.java
+++ src/main/java/com/github/dojo/caixaeletronico/CaixaEletronico.java
@@ -1,6 +1,7 @@
 package com.github.dojo.caixaeletronico;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,37 +11,45 @@ import java.util.List;
  * seguintes:
  * 
  * Entregar o menor número de notas; É possível sacar o valor solicitado com as
- * notas disponíveis; Saldo do cliente infinito; Quantidade de notas infinito
+ * notas disponíveis; 
+ * Saldo do cliente infinito; Quantidade de notas infinito
  * (pode-se colocar um valor finito de cédulas para aumentar a dificuldade do
  * problema); Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
  * 
  * Exemplos:
  * 
  * Valor do Saque: R$ 30,00 – Resultado Esperado: Entregar 1 nota de R$20,00 e 1
- * nota de R$ 10,00. Valor do Saque: R$ 80,00 – Resultado Esperado: Entregar 1
+ * nota de R$ 10,00. 
+ * Valor do Saque: R$ 80,00 – Resultado Esperado: Entregar 1
  * nota de R$50,00 1 nota de R$ 20,00 e 1 nota de R$ 10,00.
  */
 public class CaixaEletronico {
 
-    public Integer sacar(BigDecimal valorSacar) {
+    public List<Integer> sacar(int valorSacar) {
 
-
-        if (valorSacar.equals(new BigDecimal(0)) || valorSacar.intValue() < 0) {
+        List<Integer> valorSacado = new ArrayList<>();
+        
+        if (valorSacar % 10 != 0 || valorSacar <= 0) {
             throw new IllegalArgumentException();
         }
 
-        if (valorSacar.remainder(new BigDecimal(2)).intValue() != 0){
-            throw new IllegalArgumentException();
+        // if (valorSacar % 10 == 0 ){
+        //      return valorSacar / 10;              
+        // }
+
+        if (valorSacar % 100 == 0) {
+            valorSacado.add(100);
         }
+        else if (valorSacar % 50 == 0) {
+            valorSacado.add(50);
+        }
+        else if (valorSacar % 20 == 0) {
+            valorSacado.add(20);
+        }
+        else if (valorSacar % 10 == 0) {
+            valorSacado.add(10);
+        }         
 
-
-
-        BigDecimal qtdNotas =  valorSacar.divide(new BigDecimal(100));
-
-
-
-
-
-        return 10;
+        return valorSacado;
     }
 }
--- src/test/java/com/github/dojo/caixaeletronico/CaixaEletronicoTest.java
+++ src/test/java/com/github/dojo/caixaeletronico/CaixaEletronicoTest.java
@@ -5,10 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import junit.framework.Assert;
 
 /**
  * Classe de teste para Dojo do desafio Caixa Eletrônico
@@ -25,65 +24,88 @@ public class CaixaEletronicoTest {
     }
 
     @Test()
-    public void sacar_valor_10(){
-        BigDecimal valorSaque = new BigDecimal(10);
-       Assert.assertEquals(10,  caixaEletronico.sacar(valorSaque));
+    public void sacar_valor_100() {
+        int valorSaque = 100;
+        Assert.assertArrayEquals(new Integer[] { 100 }, caixaEletronico.sacar(valorSaque).toArray(new Integer[0]));
+    }
 
-    }    
+    @Test()
+    public void sacar_valor_50() {
+        int valorSaque = 50;
+        Assert.assertArrayEquals(new Integer[] { 50 }, caixaEletronico.sacar(valorSaque).toArray(new Integer[0]));
+    }   
+
+    @Test()
+    public void sacar_valor_20() {
+        int valorSaque = 20;
+        Assert.assertArrayEquals(new Integer[] { 20 }, caixaEletronico.sacar(valorSaque).toArray(new Integer[0]));
+    }
+    
+    @Test()
+    public void sacar_valor_10() {
+        int valorSaque = 10;
+        Assert.assertArrayEquals(new Integer[] { 10 }, caixaEletronico.sacar(valorSaque).toArray(new Integer[0]));
+    }  
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_envia_nota_0_indisponivel() {
+        Integer valorSaque = new Integer(0);
+        caixaEletronico.sacar(valorSaque);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_valor_invalido() {
+        Integer valorSaque = new Integer(1);
+        caixaEletronico.sacar(valorSaque);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sacar_valor_negativo() {
+        Integer valorSaque = new Integer(-1);
+        caixaEletronico.sacar(valorSaque);
+    }
+
+    // @Test()
+    // public void sacar_valor_10(){
+    // int valorSaque = 10;
+
+    // // Assert.assertEquals(10, caixaEletronico.sacar(valorSaque));
+
+    // }
 
     // @Test
     // public void sacar_sucesso() {
-    //     BigDecimal valorSaque = new BigDecimal(30);
-    //     List<Integer> notas = caixaEletronico.sacar(valorSaque);
+    // BigDecimal valorSaque = new BigDecimal(30);
+    // List<Integer> notas = caixaEletronico.sacar(valorSaque);
 
-
-        
-    //     assertEquals(valorSaque, );
-    //     assertEquals(1, notas.qtdDez);
-    //     assertEquals(0, notas.qtdCinquenta);
-    //     assertEquals(0, notas.qtdCem);
+    // assertEquals(valorSaque, );
+    // assertEquals(1, notas.qtdDez);
+    // assertEquals(0, notas.qtdCinquenta);
+    // assertEquals(0, notas.qtdCem);
     // }
 
     // @Test
     // public void sacar_envia_nota_5_indisponivel(){
 
-    //     BigDecimal valorSaque = new BigDecimal(5);
+    // BigDecimal valorSaque = new BigDecimal(5);
 
-    //     assertFalse((valorSaque % 10 == 0));
+    // assertFalse((valorSaque % 10 == 0));
     // }
 
     // @Test
     // public void sacar_envia_nota_25_indisponivel(){
 
-    //     BigDecimal valorSaque = new BigDecimal(25);
+    // BigDecimal valorSaque = new BigDecimal(25);
 
-    //     assertFalse((valorSaque % 10 == 0));
-    // }   
+    // assertFalse((valorSaque % 10 == 0));
+    // }
 
     // @Test
     // public void sacar_envia_nota_212_indisponivel(){
 
-    //     BigDecimal valorSaque = new BigDecimal(212);
+    // BigDecimal valorSaque = new BigDecimal(212);
 
-    //     assertFalse((valorSaque % 10 == 0));
-    // } 
-
-    @Test(expected = IllegalArgumentException.class)
-    public void sacar_envia_nota_0_indisponivel(){
-        BigDecimal valorSaque = new BigDecimal(0);
-        caixaEletronico.sacar(valorSaque);
-    }          
-
-    @Test(expected = IllegalArgumentException.class)
-    public void sacar_valor_decimal(){
-        BigDecimal valorSaque = new BigDecimal(1.20);
-        caixaEletronico.sacar(valorSaque);
-    }          
-
-    @Test(expected = IllegalArgumentException.class)
-    public void sacar_valor_negativo(){
-        BigDecimal valorSaque = new BigDecimal(-1);
-        caixaEletronico.sacar(valorSaque);
-    }    
+    // assertFalse((valorSaque % 10 == 0));
+    // }
 
 }
